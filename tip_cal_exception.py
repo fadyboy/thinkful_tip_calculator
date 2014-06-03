b6a885e2ae60b267908d13b136730926acdb983a
anthonyfadero <aofadero@gmail.com>
2014-06-03 22:07:57 +0100

    Final version for exception exercise

--- tip_cal_exception.py
+++ tip_cal_exception.py
@@ -1,30 +1,34 @@
 import sys
 from tip_cal_functions import calculate_rate, calculate_meal_costs
-# import pdb
 
 # display a friendly error message if a ValueError exception is raised
 
 def main():
-    done = False
-    while not done:
-        try:
-            # assign user inputs for the cost of the meal, tax rate and tip rate
-            meal = float(sys.argv[1])
-            tax_rate = float(sys.argv[2])
-            tip_rate = float(sys.argv[3])
+    # assign user supplied inputs for the cost of the meal, tax rate and tip rate
+    try:
+        meal = float(sys.argv[1])
+        tax_rate = float(sys.argv[2])
+        tip_rate = float(sys.argv[3])
 
-            done = True
+    except ValueError:
+        print "Please enter valid numbers"
 
-        except ValueError:
-            print "Please enter a valid number for all your input parameters"
-            meal = float(raw_input("Please enter a valid amount for the meal:"))
-            tax_rate = float(raw_input("Please enter a valid amount for the tax rate:"))
-            tip_rate = float(raw_input("Please enter a valid amount for the tip rate:"))
-            break
+        # assign the value False to a variable done which forms the break for a while loop if valid input entered
+        done = False
+        # prompt the user to enter valid input until valid input is supplied
+        while not done:
+            try:
+                meal = float(raw_input("Please enter a valid number for the meal:"))
+                tax_rate = float(raw_input("Please enter a valid number for the tax rate:"))
+                tip_rate = float(raw_input("Please enter a valid number for the tip rate: "))
 
+                # set the done variable to True if no exception is thrown
+                done = True
+
+            except ValueError:
+                print "You still need to enter a valid numbers! "
 
     # Calculate the meal costs and print out using imported functions
-    # pdb.set_trace()
     meal_info = calculate_meal_costs(meal, tax_rate, tip_rate)
 
     print "The base cost of your meal is ${:.2f}".format(meal_info['meal_base'])
