114d569be0ffa5015206812d7b17a60717778ef4
anthonyfadero <aofadero@gmail.com>
2014-06-04 21:42:59 +0100

    refactored code

--- exception.py
+++ exception.py
@@ -4,18 +4,32 @@ from tip_cal_functions import calculate_meal_costs, calculate_rate
 
 
 def main():
+    # assign the value False to the variable done
+    done = False
     try:
         # assign the user supplied inputs to the meal, tax_rate and tip_rate variables
         meal = float(sys.argv[1])
         tax_rate = float(sys.argv[2])
         tip_rate = float(sys.argv[3])
 
+        # if valid input from sys.argv set done to True
+        done = True
+
     except ValueError:
         # if a ValueError exception is thrown print friendly error message and prompt user for input
         print "Please enter valid numbers"
-        meal = float(raw_input("Meal cost:"))
-        tax_rate = float(raw_input("Tax rate:"))
-        tip_rate = float(raw_input("Tip rate:"))
+
+    while not done:
+        try:
+            meal = float(raw_input("Meal cost:"))
+            tax_rate = float(raw_input("Tax rate:"))
+            tip_rate = float(raw_input("Tip rate:"))
+
+            # set done to True if valid input
+            done = True
+
+        except ValueError:
+            print "You still need to enter a valid number!!"
 
     # assign the calculation of the meal costs to the variable meal_info
     meal_info = calculate_meal_costs(meal, tax_rate, tip_rate)
